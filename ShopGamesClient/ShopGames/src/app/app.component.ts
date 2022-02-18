bc1baed4328418e2872b943758edbd93fe8cf282
RhyanSPisoni <rhyanoficial14@gmail.com>
2022-02-18 17:33:39 -0300

    Att.

--- ShopGamesClient/ShopGames/src/app/app.component.ts
+++ ShopGamesClient/ShopGames/src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ShopGames';
+  public todos: Todo[] = [];
+  public form: any;
+  /**
+   *
+   */
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required,
+        ]),
+      ],
+    });
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false))
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUnDone(todo: Todo) {
+    todo.done = false;
+  }
 }
